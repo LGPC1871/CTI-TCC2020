37ca5d65c465d9cf369483d162d36ba991893e56
Luis Gustavo Pinheiro da Cunha <lgpc1871@gmail.com>
2020-04-27 23:52:34 -0300

    Preparando Template

---
+++ web/application/views/template/footer.php
@@ -0,0 +1,8 @@
+<footer>
+    <div class="footer-copyright text-center" id="footer-text">
+        <p>Projeto COTIL Jogos</p>
+    </div>
+</footer>
+</body>
+</html>
+
---
+++ web/application/views/template/head.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+$diretorio = base_url();
+$templateStyles = array("body.css", "header.css", "main.css", "footer.css");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!--JQuery-->
+    <script type="text/javascript" src="vendor/components/jquery/jquery.js"></script>
+    <!--Bootstrap-->
+    <script type="text/javascript" src="vendor/twbs/bootstrap/dist/js/bootstrap.js"></script>
+    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.css" />
+    <!--Template Styles-->
+    <?php if(isset($templateStyles)): ?>
+        <?php foreach($templateStyles as $style_name): ?>
+            <?php $href ="public/css/template/" . $style_name ?>
+            <link href="<?=$href?>" rel="stylesheet" />
+        <?php endforeach ?>
+    <?php endif ?>
+    <title>COTIL Jogos</title>
+</head>
+<body>
