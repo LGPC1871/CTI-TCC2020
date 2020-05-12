8174e92f548540e183f4efd69d69de95caabdec0
Luis Gustavo Pinheiro da Cunha <lgpc1871@gmail.com>
2020-05-11 23:14:03 -0300

    Adicionando função model para jumbotron

--- web/application/controllers/Home.php
+++ web/application/controllers/Home.php
@@ -6,16 +6,18 @@ class Home extends CI_Controller{
         parent::__construct();
         $this->load->model('Home_content');
     }
+    
     public function index(){
         redirect('home/main');
     }
 
     public function main(){
         $carrosselArray = $this->Home_content->selectCarrosselData();
-
+        $jumbotronArray = $this->Home_content->selectJumbotronData();
         $content = array(
             "styles" => array('carrossel.css', 'jumbotron.css'),
-            "carrossel" => $carrosselArray
+            "carrossel" => $carrosselArray,
+            "jumbotron" => $jumbotronArray
         );
         $this->template->show("home.php", $content);
     }
