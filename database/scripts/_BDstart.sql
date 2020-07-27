0a64998e36334afe8616d4d42707477ab55b862c
Luis Gustavo Pinheiro da Cunha <lgpc1871@gmail.com>
2020-07-27 10:09:45 -0300

    Update _BDstart.sql

--- database/scripts/_BDstart.sql
+++ database/scripts/_BDstart.sql
@@ -10,7 +10,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema cl19467
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `cl19467` ;
+/*DROP SCHEMA IF EXISTS `cl19467` ;*/
 
 -- -----------------------------------------------------
 -- Schema cl19467
