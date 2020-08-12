bc7035788eff670ca87c3a224f7eaadd7b6a4fc1
Luis Gustavo Pinheiro da Cunha <lgpc1871@gmail.com>
2020-08-12 15:23:55 -0300

    Create loading.php

---
+++ web/application/views/template/loading.php
@@ -0,0 +1,7 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<div class="text-center text-nowrap">
+    <h5><i class="fas fa-circle-notch fa-spin"></i>&nbsp Carregando...</h5>
+</div>
